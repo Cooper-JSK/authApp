4d4a09089e6315b1e6eabe637ec0113a9d8c0e7c
CryoWarlocK <karunarathna_js_e23@engug.ruh.ac.lk>
2024-06-03 20:20:12 +0530

    add routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home.jsx'
+import About from './pages/About.jsx'
+import SignIn from './pages/SignIn.jsx'
+import SignUp from './pages/SignUp.jsx'
+import Profile from './pages/Profile.jsx'
+
 
 const App = () => {
   return (
-    <div className='text-red-400'>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/profile' element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
